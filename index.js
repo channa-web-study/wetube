8d46661ef21b7c5d4fa57730e6cf8d95722fbc27
chan-na <kimhim7025@gmail.com>
2020-06-07 16:04:06 +0900

    babel nodemon

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const express = require("express");
-const app = express();
+import express from "express";
 
 const PORT = 4000;
 
-function handleListening() {
+const app = express();
+
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
-  console.log(req);
-  res.send("Hello from Home");
-}
+const handleHome = (req, res) => res.send("Hello from Home");
 
-function handleProfile(req, res) {
-  res.send("Hello from Profile");
-}
+const handleProfile = (req, res) => res.send("Hello from Profile");
 
 app.get("/", handleHome);
 
