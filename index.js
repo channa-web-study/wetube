da09a2c2ba12ae3acfc24b5d88c26d69ebe5b537
chan-na <kimhim7025@gmail.com>
2020-06-07 21:56:21 +0900

    add morgan, helmet, body-parser, cookie-parser

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const PORT = 4000;
 
@@ -11,12 +15,11 @@ const handleHome = (req, res) => res.send("Hello from Home");
 
 const handleProfile = (req, res) => res.send("Hello from Profile");
 
-const middleware = (req, res, next) => {
-  console.log("Beween Home");
-  next();
-};
-
-app.use(middleware);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
